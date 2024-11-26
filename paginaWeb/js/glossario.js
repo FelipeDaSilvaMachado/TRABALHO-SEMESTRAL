c9cd667aa93f54730a9aed887b0c50bddfc32b14
FelipeDaSilvaMachado <felipe.silvamachado@yahoo.com>
2024-11-26 20:07:35 -0300

    Commit final

--- paginaWeb/js/glossario.js
+++ paginaWeb/js/glossario.js
@@ -1,4 +1,6 @@
-document.querySelectorAll('.btnPalavra').forEach(button => { /* Seleciona todos os elementos da classe */
-    const audio = new Audio(button.dataset.audio); 
-    button.onmouseover = () => audio.play(); // Toca o áudio quando o mouse passa
+document.querySelectorAll('.btnPalavra').forEach(button => {
+    button.addEventListener('click', function() {
+        const audio = this.querySelector('audio');
+        audio.play(); // Toca o áudio
+    });
 });
